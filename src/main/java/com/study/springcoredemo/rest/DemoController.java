17b3d3918eb205dd07484e1c8819cb75659c9d59
thangnd <thangit96@gmail.com>
2024-03-10 14:44:06 +0700

    new code

--- src/main/java/com/study/springcoredemo/rest/DemoController.java
+++ src/main/java/com/study/springcoredemo/rest/DemoController.java
@@ -11,17 +11,30 @@ public class DemoController {
 
     // define a private field for dependency
     private Coach myCoach;
+    private Coach anotherCoach;
 
     // define a constructor for dependency injection
 
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach,
+                          @Qualifier("cricketCoach") Coach theAnotherCoach) {
         System.out.println("In constructor:" + getClass().getSimpleName());
         myCoach = theCoach;
+        anotherCoach = theAnotherCoach;
     }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return myCoach.getDailyworkout();
     }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
+    }
 }
+
+
+
+
+
